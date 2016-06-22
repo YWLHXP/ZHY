0e80ecf6274437db8e82eed30661561d1858f152
peng <xpdragon1993@163.com>
2016-06-22 12:49:07 +0800

    初始化项目，增加cocoaPods支持

---
+++ ZuiHuiYou/ZuiHuiYou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZuiHuiYou
+//
+//  Created by dragon on 16/6/22.
+//  Copyright © 2016年 win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
